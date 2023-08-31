ea6cfc728151a25c6172cf4862827c32415966c1
Priyasingh098 <singh412priya@gmail.com>
2023-08-31 15:46:29 +0530

    first commit

---
+++ src/main/java/com/demo/controller/democontroller.java
@@ -0,0 +1,16 @@
+package com.demo.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class democontroller {
+	@GetMapping("/get")
+	public String show() {
+		return "Welcome to amdocs spring boot session.";
+	}
+	@GetMapping("/set")
+	public String show1() {
+		return "Great you got it.";
+	}
+
+}
